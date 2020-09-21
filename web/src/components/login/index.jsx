3dd6a90e7f0219d3b3f1c46163cbf687709f5815
Rubens Praser Júnior <praser@gmail.com>
2020-09-21 16:51:41 -0300

    Integração da tela de login com a api

--- web/src/components/login/index.jsx
+++ web/src/components/login/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import {
   Container,
@@ -12,22 +13,47 @@ import {
   Divider,
   Link,
 } from './styles';
+import { login } from '../../utils/login';
+import { routes } from '../../utils/routes';
 import image from '../../assets/undraw_Login_v483.svg';
 
 export const Login = () => {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const history = useHistory();
+
+  const handleUsernameBlur = event => setUsername(event.target.value);
+  const handlePasswordBlur = event => setPassword(event.target.value);
+
+  const handleLoginClick = async event => {
+    event.preventDefault();
+    await login(username, password);
+    history.push(routes.home);
+  };
+
   return (
     <Container>
       <Content>
         <LoginImage src={image} />
         <Form>
           <Title>Bem vindo de volta!</Title>
-          <Input type="text" placeholder="Digite a sua matrícula" />
-          <Input type="password" placeholder="Digite a sua senha" />
-          <ButtonPrimary>Login</ButtonPrimary>
+          <Input
+            type="text"
+            placeholder="Digite a sua matrícula"
+            onBlur={handleUsernameBlur}
+          />
+          <Input
+            type="password"
+            placeholder="Digite a sua senha"
+            onBlur={handlePasswordBlur}
+          />
+          <ButtonPrimary onClick={handleLoginClick}>Login</ButtonPrimary>
           <Divider />
           <ButtonDanger>Ir para a página da SUDEP </ButtonDanger>
           <Divider />
-          <Link href={process.env.PAGINA_SUDEP}>Não consegue acessar?</Link>
+          <Link href={process.env.REACT_APP_MAIN_PORTAL}>
+            Não consegue acessar?
+          </Link>
         </Form>
       </Content>
     </Container>
---
+++ web/src/utils/apis/authApi.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const options = {
+  baseURL: process.env.REACT_APP_AUTH_API_URL,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+};
+
+const client = axios.create(options);
+
+export const authenticate = async (username, password) => {
+  return client.post('/authenticate', {
+    credentials: { username, password },
+  });
+};
