0ad40ec41322e0bca669c03e61806c0272cf5872
Rubens Praser Júnior <praser@gmail.com>
2020-09-23 12:03:33 -0300

    Extração das rotas do menu para o arquivo paths

--- web/src/components/Navigation/index.jsx
+++ web/src/components/Navigation/index.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
-import { faFileSignature } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBuilding,
+  faDollarSign,
+  faFileSignature,
+} from '@fortawesome/free-solid-svg-icons';
 
 import Brand from './Brand';
 import Collapse from './Collapse';
 import { Divider, Navbar, SectionTitle } from '~/components/Navigation/styles';
 
+import {
+  predictedBlockPath,
+  blockedPath,
+  canceledPath,
+  createUgPath,
+  createCommitmentPath,
+  ugPath,
+  joinPath,
+} from '~/utils/paths';
+import { join } from 'lodash';
+
 const Navigation = () => {
   return (
     <Navbar>
       <Brand />
+
+      <Divider />
+
       <Collapse
         label="Safra 2017"
         icon={faFileSignature}
         collapsables={[
-          { label: 'Prévia do bloqueio', to: '/safras/2017/previa-bloqueio' },
-          { label: 'Empenhos bloqueados', to: '/safras/2017/bloqueios' },
-          { label: 'Empenhos cancelados', to: '/safras/2017/cancelamentos' },
+          {
+            label: 'Prévia do bloqueio',
+            to: joinPath(predictedBlockPath, [2017]),
+          },
+          { label: 'Empenhos bloqueados', to: joinPath(blockedPath, [2017]) },
+          { label: 'Empenhos cancelados', to: joinPath(canceledPath, [2017]) },
         ]}
       />
 
@@ -23,12 +44,31 @@ const Navigation = () => {
         label="Safra 2018"
         icon={faFileSignature}
         collapsables={[
-          { label: 'Prévia do bloqueio', to: '/safras/2017/previa-bloqueio' },
+          {
+            label: 'Prévia do bloqueio',
+            to: joinPath(predictedBlockPath, [2018]),
+          },
         ]}
       />
+
       <Divider />
 
       <SectionTitle>Configuraçãoes</SectionTitle>
+
+      <Collapse
+        label="UGs"
+        icon={faBuilding}
+        collapsables={[
+          { label: 'Listar', to: ugPath },
+          { label: 'Cadastrar', to: createUgPath },
+        ]}
+      />
+
+      <Collapse
+        label="Saldos NE"
+        icon={faDollarSign}
+        collapsables={[{ label: 'Atualizar', to: createCommitmentPath }]}
+      />
     </Navbar>
   );
 };
--- web/src/utils/paths.js
+++ web/src/utils/paths.js
@@ -1,4 +1,35 @@
+import { matches } from 'lodash';
+
 export const dashboardPath = '/dashboard';
 export const homePath = '/';
 export const loginPath = '/login';
 export const logouPath = '/logout';
+export const predictedBlockPath = '/safras/:year/previa-bloqueio';
+export const blockedPath = '/safras/:year/bloqueios';
+export const canceledPath = '/safras/:year/cancelamentos';
+export const ugPath = '/ugs';
+export const createUgPath = '/ugs/novo';
+export const createCommitmentPath = '/notas-empenho/saldo/novo';
+
+const replaceParams = (path, from, to) => {
+  let result = path.repeat(1);
+  from.map((value, i) => {
+    result = result.replaceAll(value, to[i]);
+  });
+  return result;
+};
+
+const validateParamsToReplace = (from, to) => {
+  if (from.length === to.length) return true;
+
+  throw new Error(
+    `Wrong amoun of elements in params array. Expected ${from.length} and received ${to.length}`,
+  );
+};
+
+export const joinPath = (path, params) => {
+  const re = /:\w*/gm;
+  const matches = path.match(re);
+  validateParamsToReplace(matches, params);
+  return replaceParams(path, matches, params);
+};
