213717a01265e65cb5a115e6d182820e07d9ea3f
Rubens Praser Júnior <praser@gmail.com>
2020-09-23 05:15:30 -0300

    Criação do component Navbar

---
+++ web/src/components/Navigation/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Collapse = styled.div``;
+
+export const Divider = styled.hr``;
+export const Link = styled.a``;
+
+export const Navbar = styled.div`
+  width: 14rem;
+  min-height: 100vh;
+  background-color: #36b9cc;
+  background-image: linear-gradient(180deg, #36b9cc 10%, #258391 100%);
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SectionTitle = styled.p``;
