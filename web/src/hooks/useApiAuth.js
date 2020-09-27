3e9f6ff124529bece921b36269db11b1448485ec
Rubens Praser Júnior <praser@gmail.com>
2020-09-27 09:37:20 -0300

    Padronização da definição do endpoint de autenticação no useApiAuth

--- web/src/hooks/useApiAuth.js
+++ web/src/hooks/useApiAuth.js
@@ -3,13 +3,13 @@ import getXhrClient from '../utils/xhrClient';
 const useApiAuth = async () => {
   const client = await getXhrClient(process.env.REACT_APP_AUTH_API_URL, false);
 
-  return {
-    postAuthenticate: async (username, password) => {
-      return client.post('/authenticate', {
-        credentials: { username, password },
-      });
-    },
+  const postAuthenticate = async (username, password) => {
+    return client.post('/authenticate', {
+      credentials: { username, password },
+    });
   };
+
+  return { postAuthenticate };
 };
 
 export default useApiAuth;
