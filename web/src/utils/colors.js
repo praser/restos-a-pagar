1e9358d8074ad786b5eb351b1e29cfa64ee310f8
Rubens Praser Júnior <praser@gmail.com>
2020-10-07 07:55:01 -0300

    Adiciona a variante danger ao esquema de cores

--- web/src/utils/colors.js
+++ web/src/utils/colors.js
@@ -2,6 +2,7 @@ const success = '#1cc88a ';
 const info = '#36b9cc';
 const warning = '#f6c23e';
 const primary = '#4e73df';
+const danger = '#ea4335';
 
 export const handleVariant = variant => {
   switch (variant) {
@@ -11,6 +12,8 @@ export const handleVariant = variant => {
       return info;
     case 'warning':
       return warning;
+    case 'danger':
+      return danger;
     default:
       return primary;
   }
