e2d00f2a3641961ed06412f2ba50639c10707761
Rubens Praser Júnior <praser@gmail.com>
2021-01-15 13:47:52 -0300

    Correção no payload da autenticação

--- web/src/hooks/useApiAuth.js
+++ web/src/hooks/useApiAuth.js
@@ -4,10 +4,10 @@ const useApiAuth = async () => {
   const client = await getXhrClient(process.env.REACT_APP_AUTH_API_URL, false);
 
   const postAuthenticate = async () => {
-    const req = await fetch('http://localhost:2004/sistemas/user');
+    const req = await fetch(process.env.REACT_APP_USER_URL);
     const json = await req.json();
     const { id: username } = json;
-    return client.post('/authenticate/username', { username });
+    return client.post('/authenticate/username', { credentials: { username } });
   };
 
   return { requests: { postAuthenticate } };
