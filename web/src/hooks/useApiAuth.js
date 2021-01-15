30f891376769790344cd7f39538f58774f3307ec
Rubens Praser Júnior <praser@gmail.com>
2021-01-15 11:00:09 -0300

    Ajustes na autenticação do usuário

--- web/src/hooks/useApiAuth.js
+++ web/src/hooks/useApiAuth.js
@@ -4,7 +4,10 @@ const useApiAuth = async () => {
   const client = await getXhrClient(process.env.REACT_APP_AUTH_API_URL, false);
 
   const postAuthenticate = async () => {
-    return client.post('/authenticate/username');
+    const req = await fetch('http://localhost:2004/sistemas/user');
+    const json = await req.json();
+    const { id: username } = json;
+    return client.post('/authenticate/username', { username });
   };
 
   return { requests: { postAuthenticate } };
